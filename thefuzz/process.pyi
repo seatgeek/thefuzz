b925a872b29393081caa21cb1be17768734b9b10
Harry Lees <harry.lees@gmail.com>
2022-01-25 01:03:23 +0000

    fixed extractWithoutOrder type hint

--- thefuzz/process.pyi
+++ thefuzz/process.pyi
@@ -1,9 +1,9 @@
 from collections.abc import Mapping
 import typing
-from typing import Any, Callable, Iterable, Union, Tuple, Generator, TypeVar
+from typing import Any, Callable, Union, Tuple, Generator, TypeVar, Sequence
 
 
-ChoicesT = Union[Mapping[str, str], Iterable[str]]
+ChoicesT = Union[Mapping[str, str], Sequence[str]]
 T = TypeVar('T')
 ProcessorT = Union[Callable[[str, bool], str], Callable[[Any], Any]]
 ScorerT = Callable[[str, str, bool, bool], int]
@@ -14,4 +14,4 @@ def extractWithoutOrder(query: str, choices: Mapping[str, str], processor: Proce
 
 
 @typing.overload
-def extractWithoutOrder(query: str, choices: Iterable[str], processor: ProcessorT, scorer: ScorerT, score_cutoff: int = ...) -> Generator[Tuple[str, int], None, None]: ...
+def extractWithoutOrder(query: str, choices: Sequence[str], processor: ProcessorT, scorer: ScorerT, score_cutoff: int = ...) -> Generator[Tuple[str, int], None, None]: ...
